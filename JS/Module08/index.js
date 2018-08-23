0e4a30c72d757d455697fec8e5443cba020272fc
Tiragesta <eugenrashupkina@gmail.com>
2018-08-24 00:15:14 +0300

    new module8

--- JS/Module08/index.js
+++ JS/Module08/index.js
@@ -1,62 +1,50 @@
 'use strict';
-
 const galleryItems = [
     { 
-      preview: 'img/preview-1.jpg', 
-      fullview: 'img/fullview-1.jpg', 
+      previews: 'img/preview-1.jpg', 
+      fullviews: 'img/fullview-1.jpg', 
       alts: "alt text 1" 
     },
     { 
-      preview: 'img/preview-2.jpg', 
-      fullview: 'img/fullview-2.jpg', 
+      previews: 'img/preview-2.jpg', 
+      fullviews: 'img/fullview-2.jpg', 
       alts: "alt text 2" 
     },
     {
-      preview: 'img/preview-3.jpg', 
-      fullview: 'img/fullview-3.jpg', 
+      previews: 'img/preview-3.jpg', 
+      fullviews: 'img/fullview-3.jpg', 
       alts: "alt text 3" 
     },
-     { 
-      preview: 'img/preview-4.jpg', 
-      fullview: 'img/fullview-4.jpg', 
+    { 
+      previews: 'img/preview-4.jpg', 
+      fullviews: 'img/fullview-4.jpg', 
       alts: "alt text 4" 
-     },
+    }, 
    { 
-      preview: 'img/preview-5.jpg', 
-      fullview: 'img/fullview-5.jpg', 
+      previews: 'img/preview-5.jpg', 
+      fullviews: 'img/fullview-5.jpg', 
       alts: "alt text 5" 
     },
     { 
-      preview: 'img/preview-6.jpg', 
-      fullview: 'img/fullview-6.jpg', 
+      previews: 'img/preview-6.jpg', 
+      fullviews: 'img/fullview-6.jpg', 
       alts: "alt text 6" 
     }
 ];
+//console.log(galleryItems);
 
-/*<div class="fullview">
-           Если выбран первый элемент из preview 
-          <img class="full_img" src="img/fullview-1.jpeg" alt="alt text 1" width="1280" height="853">
-        </div>
-           li будет столько, сколько объектов в массиве картинок. Эти 3 для примера 
-        <ul class="preview">
-          <li><img class="pre_img" src="img/preview-2.jpg" data-fullview="img/fullview-2.jpeg" alt="alt text 2"></li>
-          <li><img class="pre_img" src="img/preview-3.jpg" data-fullview="img/fullview-3.jpeg" alt="alt text 3"></li>
-          <li><img class="pre_img" src="img/preview-4.jpg" data-fullview="img/fullview-4.jpeg" alt="alt text 4"></li>
-          <li><img class="pre_img" src="img/preview-5.jpg" data-fullview="img/fullview-5.jpeg" alt="alt text 5"></li>
-        </ul> */
-
-const createFullView = (fullview, alts) => {
-    const fullviews = document.createElement('div');
-    fullviews.classList.add('fullview');
+const createFullView = ({fullviews, alts}) => {
+    const fullview = document.createElement('div');
+    fullview.classList.add('fullview');
 
     const img = document.createElement('img');
-    img.setAttribute('src', fullview);
+    img.setAttribute('src', fullviews);
     img.setAttribute('alt', alts);
     img.setAttribute('width', 1280);
 
-    fullviews.append(img);
+    fullview.append(img);
 
-    return fullviews;
+    return fullview;
 };
 
 const createPreview = ( ) => {
@@ -66,13 +54,13 @@ const createPreview = ( ) => {
     return ul;
 };
 
-const createPreviewImage = ( {preview, fullview, alts}) => {
+const createPreviewImage = ( {previews, fullviews, alts}) => {
     const li = document.createElement('li');
 
     const image = document.createElement('img');
     image.classList.add('pre_img');
-    image.setAttribute('src', preview);
-    image.setAttribute('data-fullview', fullview);
+    image.setAttribute('src', previews);
+    image.setAttribute('data-fullview', fullviews);
     image.setAttribute('alt', alts);
     image.setAttribute('width', 320);
 
@@ -81,66 +69,40 @@ const createPreviewImage = ( {preview, fullview, alts}) => {
     return li;
 }
 
-const createGallery = ({preview, fullview, alts}) => {
-    const maingallery = document.createElement('div');
-    maingallery.classList.add('maingallery');
-    const imageGallery = document.querySelector('.js-image-gallery');
-
-    const fullviews = createFullView(fullview, alts);
-
-    const ul = createPreview();
-
-    const li = createPreviewImage({preview, fullview, alts});
-    
-    ul.append(li);
-    
-    maingallery.append(fullviews, ul);
-
-    return maingallery;
-}
-
-const createPreviews = (galleryItems) => {
+const createImageList = (galleryItems) => {
     const elements = galleryItems.map(galleryItem => createPreviewImage(galleryItem));
 
     return elements;
 };
+
 const imageGallery = document.querySelector('.js-image-gallery');
-const gallery = createGallery(galleryItems[0]);
 
-imageGallery.appendChild(gallery);
+const previewList = createPreview();
+const fullviewList = createFullView(galleryItems[0]);
+imageGallery.append(fullviewList, previewList);
 
-const smallGallery = document.querySelector('.preview')
-const smallGalleryItem = createPreviews(galleryItems)
 
-smallGallery.append(...smallGalleryItem);
+
+const smallImageItem = createImageList(galleryItems)
+
+
+previewList.append(...smallImageItem);
+
+const previewImg = document.querySelector('.preview');
+previewImg.addEventListener('click', handlePreviewImgClick);
+
+function handlePreviewImgClick(evt){
+    evt.preventDefault();
    
- 
+   const nodeName = evt.target.nodeName;
+   const action = evt.target.dataset.fullview;
+   //console.log(action);
 
-const smallImgs = imageGallery.querySelectorAll('li.pre_img');
-const smallActiveImg = smallImgs[0].classList.add('pre-active-img');
+   if(nodeName !== 'IMG') return;
+   
+   const activeLargeImg = imageGallery.querySelector('.fullview img');
 
-smallImgs.addEventListener('click', handleNewImg);
+  activeLargeImg.setAttribute('src', action);
+};
 
-function handleNewImg({target}){
-
-  const hasClass = target.classList.contains('pre-img');
-
-  if (!hasClass) return;
-
-  const activeLargeImg = imageGallery.querySelector('.fullview img');
-
-  activeLargeImg.setAttribute('src', target.dataset.fullview);
-
-  setActiveSmallImg(smallImgs, target);
-}
-
-function setActiveSmallImg(smallImgs, target){
-  smallImgs.forEach(smallImg => {
-    if(smallImg !== target) {
-      smallImg.classList.remove('pre-active-img');
-    } else {
-      smallImg.classList.add('pre-active-img');
-    }
-  });
-}
 
